b56f8755ede990276bd783025feef5523e3b8eab
ogaihtorecic <cicero.thiago@gmail.com>
2014-01-19 19:04:59 -0400

    Ajuste do leiaute da tela de apostas

--- src/io/apiary/megasena/ApostaAdapter.java
+++ src/io/apiary/megasena/ApostaAdapter.java
@@ -42,63 +42,84 @@ public class ApostaAdapter extends BaseAdapter {
 
 		Aposta aposta = this.listAposta.get(position);
 		ViewHolder viewHolder;
-		LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
-		if(convertView != null) {
+		LayoutInflater layoutInflater = (LayoutInflater) this.context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		if (convertView != null) {
 			viewHolder = (ViewHolder) convertView.getTag();
-			
+
 		} else {
-			
-			convertView = layoutInflater.inflate(R.layout.activity_historico_aposta_item, null);
-			
+
+			convertView = layoutInflater.inflate(
+					R.layout.activity_historico_aposta_item, null);
+
 			viewHolder = new ViewHolder();
-			
-			viewHolder.tvNumConcurso = (TextView) convertView.findViewById(R.id.tv_num_concurso);
+
+			viewHolder.tvNumConcurso = (TextView) convertView
+					.findViewById(R.id.tv_num_concurso);
 
 			// cria textViews e os inicializa com texto vazio
 			for (int i = 0; i < viewHolder.ids.length; i++) {
-				viewHolder.tvArray[i] = (TextView) convertView.findViewById(viewHolder.ids[i]);
+				viewHolder.tvArray[i] = (TextView) convertView
+						.findViewById(viewHolder.ids[i]);
 				viewHolder.tvArray[i].setText("");
 			}
-			
+
+			viewHolder.tvNumConcurso.setText(fillWithZeros(aposta.getConcurso()
+					.toString()));
 			// preenche de 6 a 15 textViews para serem apresentados na tela
 			Set<Integer> setDezenas = aposta.getDezenas();
 			int i = 0;
-			for (Integer dezena: setDezenas) {
-				
-				System.out.println("Dezena recuperada do BD: "+ dezena.toString());
-				viewHolder.tvArray[i++].setText(dezena.toString());
-				viewHolder.tvNumConcurso.setText(aposta.getConcurso().toString());
+			for (Integer dezena : setDezenas) {
+				String textoDezena = dezena.toString();
+				if (dezena < 10) {
+					StringBuilder stringBuilder = new StringBuilder();
+					stringBuilder.append("0");
+					stringBuilder.append(dezena.toString());
+					textoDezena = stringBuilder.toString();
+				}
+				viewHolder.tvArray[i++].setText(textoDezena);
 			}
-			
+
 			// transforma para invisivel os textViews nao preenchidos
-			for (int j = 0; j < viewHolder.tvArray.length; j++) {				
-				if("".equals(viewHolder.tvArray[j].getText())) {
+			for (int j = 0; j < viewHolder.tvArray.length; j++) {
+				if ("".equals(viewHolder.tvArray[j].getText())) {
 					viewHolder.tvArray[j].setVisibility(View.INVISIBLE);
 				}
 			}
-			
+
 			convertView.setTag(viewHolder);
 		}
-		
+
 		return convertView;
 	}
 
-	static class ViewHolder {
-		
-		private TextView tvNumConcurso, tvDez1, tvDez2, tvDez3, tvDez4, tvDez5, 
-		                 tvDez6, tvDez7, tvDez8, tvDez9, tvDez10, 
-		                 tvDez11, tvDez12, tvDez13, tvDez14, tvDez15;
+	private String fillWithZeros(String s) {
+		final int STRING_SIZE = 4;
 
-		private int[] ids = new int[] { R.id.textView1, R.id.textView2, R.id.textView3, 
-										R.id.textView4, R.id.textView5, R.id.textView6,
-										R.id.textView7, R.id.textView8, R.id.textView9,
-										R.id.textView10, R.id.textView11, R.id.textView12,
-										R.id.textView13, R.id.textView14, R.id.textView15 };
-		
-		private TextView[] tvArray = new TextView[] { tvDez1, tvDez2, tvDez3, tvDez4, tvDez5, 
-				                                      tvDez6, tvDez7, tvDez8, tvDez9, tvDez10, 
-				                                      tvDez11, tvDez12, tvDez13, tvDez14, tvDez15 };
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("000");
+		stringBuilder.append(s);
+
+		return stringBuilder.substring(stringBuilder.length() - STRING_SIZE);
+	}
+
+	static class ViewHolder {
+
+		private TextView tvNumConcurso, tvDez1, tvDez2, tvDez3, tvDez4, tvDez5,
+				tvDez6, tvDez7, tvDez8, tvDez9, tvDez10, tvDez11, tvDez12,
+				tvDez13, tvDez14, tvDez15;
+
+		private int[] ids = new int[] { R.id.label_Concurso,
+				R.id.label_Dezenas, R.id.textView3, R.id.textView4,
+				R.id.textView5, R.id.textView6, R.id.textView7, R.id.textView8,
+				R.id.textView9, R.id.textView10, R.id.textView11,
+				R.id.textView12, R.id.textView13, R.id.textView14,
+				R.id.textView15 };
+
+		private TextView[] tvArray = new TextView[] { tvDez1, tvDez2, tvDez3,
+				tvDez4, tvDez5, tvDez6, tvDez7, tvDez8, tvDez9, tvDez10,
+				tvDez11, tvDez12, tvDez13, tvDez14, tvDez15 };
 	}
 
 }
--- src/io/apiary/megasena/HistoricoApostasActivity.java
+++ src/io/apiary/megasena/HistoricoApostasActivity.java
@@ -6,35 +6,22 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class HistoricoApostasActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_historico_apostas);
-		
-		TextView tvNumConcurso = (TextView) this.findViewById(R.id.lb_num_concurso);
-		TextView tvDezenas = (TextView) this.findViewById(R.id.lb_dezenas);
 
-		ArrayList<Aposta> arrayListAposta = (ArrayList<Aposta>) getIntent().getExtras().get(Aposta.INTENT_KEY); 
-				
-		/*
-		for (Employee employee : arrayListEmployee) {
-			Log.d("EmployeeListActivity", employee.getName());
-			Log.d("EmployeeListActivity", employee.getLastName());
-			Log.d("EmployeeListActivity", employee.getPassword());
-		}*/
-				
-		ListView listView = (ListView) findViewById(R.id.listView);
-
+		ArrayList<Aposta> arrayListAposta = (ArrayList<Aposta>) getIntent()
+				.getExtras().get(Aposta.INTENT_KEY);
 		ApostaAdapter apostaAdapter = new ApostaAdapter(arrayListAposta, this);
-		
+
+		ListView listView = (ListView) findViewById(R.id.listView);
 		listView.setAdapter(apostaAdapter);
-		
+
 	}
 
 }
